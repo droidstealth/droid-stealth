1998b018b32715d2e5e7f7adf3cc97f2df38e4ff
Alex <a_kolpa@live.nl>
2014-03-06 23:49:58 +0100

    Added license to CheckableImageView

--- AndroidStealth/src/main/java/content/CheckableImageView.java
+++ AndroidStealth/src/main/java/content/CheckableImageView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Chris Banes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package content;
 
 import android.content.Context;
@@ -5,41 +20,48 @@ import android.util.AttributeSet;
 import android.widget.Checkable;
 import android.widget.ImageView;
 
-/**
- * Created by Alex on 3/6/14.
- */
 public class CheckableImageView extends ImageView implements Checkable {
-    private boolean mChecked;
 
-    private static final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
+    public CheckableImageView(Context context) {
+        super(context);
+    }
 
-    public CheckableImageView(final Context context, final AttributeSet attrs) {
+    public CheckableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    @Override
-    public int[] onCreateDrawableState(final int extraSpace) {
-        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
-        if (isChecked())
-            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
-        return drawableState;
+    private static final int[] CheckedStateSet = {android.R.attr.state_checked};
+
+    private boolean mChecked = false;
+
+    public boolean isChecked() {
+        return mChecked;
     }
 
-    @Override
+    public void setChecked(boolean b) {
+        if (b != mChecked) {
+            mChecked = b;
+            refreshDrawableState();
+        }
+    }
+
     public void toggle() {
         setChecked(!mChecked);
     }
 
     @Override
-    public boolean isChecked() {
-        return mChecked;
+    public int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CheckedStateSet);
+        }
+        return drawableState;
     }
 
     @Override
-    public void setChecked(final boolean checked) {
-        if (mChecked == checked)
-            return;
-        mChecked = checked;
-        refreshDrawableState();
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        invalidate();
     }
+
 }
