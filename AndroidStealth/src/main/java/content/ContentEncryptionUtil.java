925d99b1dfdd713594f0d07e67e202f4c97fde54
Alex Walterbos <atw.231@gmail.com>
2014-03-19 11:46:15 +0100

    Added copyFile method that uses encryption, not finished nor tested

--- AndroidStealth/src/main/java/content/ContentEncryptionUtil.java
+++ AndroidStealth/src/main/java/content/ContentEncryptionUtil.java
@@ -4,4 +4,5 @@ package content;
  * Created by awalterbos on 3/19/14.
  */
 public class ContentEncryptionUtil {
+	public static byte
 }
--- AndroidStealth/src/main/java/content/ContentManager.java
+++ AndroidStealth/src/main/java/content/ContentManager.java
@@ -1,10 +1,18 @@
 package content;
 
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -30,7 +38,7 @@ public class ContentManager implements IContentManager {
 	 * @param destFile
 	 * @throws IOException
 	 */
-	private static void copyFile(File sourceFile, File destFile) throws IOException {
+	private static void copyFileWithoutEncryption(File sourceFile, File destFile) throws IOException {
 		if (!destFile.exists()) {
 			destFile.createNewFile();
 		}
@@ -53,6 +61,38 @@ public class ContentManager implements IContentManager {
 		}
 	}
 
+	private static void copyFileWithEncryption(File sourceFile, File destFile) throws IOException {
+		if(!destFile.exists()){
+			destFile.createNewFile();
+		}
+
+		FileChannel source = null;
+		WritableByteChannel destination = null;
+
+		try{
+			source = new FileInputStream(sourceFile).getChannel();
+			destination = Channels.newChannel(new CipherOutputStream(new FileOutputStream(destFile), Cipher.getInstance("CBC")));
+
+			source.transferTo(0, source.size(), destination);
+		}
+		catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+		finally{
+			if(source != null){
+				source.close();
+			}
+			if(destination != null){
+				destination.close();
+			}
+		}
+	}
+
+
+
 	@Override
 	public Collection<ContentItem> getStoredContent() {
 		File[] files = mDataDir.listFiles();
@@ -68,7 +108,7 @@ public class ContentManager implements IContentManager {
 	@Override
 	public boolean addItem(File item) {
 		try {
-			copyFile(item, new File(mDataDir, item.getName()));
+			copyFileWithoutEncryption(item, new File(mDataDir, item.getName()));
 			notifyListeners();
 			return true;
 		}
