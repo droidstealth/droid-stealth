ba8fc9442fd6478bc3256f438d3fa84bf7e7488b
Alex Walterbos <atw.231@gmail.com>
2014-03-30 23:01:00 +0200

    Moved binding logic to onPause and onResume

--- AndroidStealth/src/main/java/com/stealth/android/HomeActivity.java
+++ AndroidStealth/src/main/java/com/stealth/android/HomeActivity.java
@@ -14,38 +14,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import android.widget.Toast;
 import content.ContentFragment;
-
 import content.EncryptionService;
 import sharing.APSharing.APSharing;
 import sharing.SharingUtils;
-import spikes.stealthdialer.StealthDialReceiver;
 
 public class HomeActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+	/**
+	 * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+	 */
+	private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
-
-    private APSharing mSharing;
-
+	/**
+	 * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+	 */
+	private CharSequence mTitle;
+	private APSharing mSharing;
 	private EncryptionService mEncryptionService;
-
 	private boolean mIsBound;
 
 	private ServiceConnection mConnection = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-			mEncryptionService = ((EncryptionService.ServiceBinder)iBinder).getService();
+			mEncryptionService = ((EncryptionService.ServiceBinder) iBinder).getService();
 		}
 
 		@Override
@@ -54,53 +47,58 @@ public class HomeActivity extends ActionBarActivity
 		}
 	};
 
-	void doBindService(){
+	void doBindService() {
 		bindService(new Intent(HomeActivity.this, EncryptionService.class), mConnection, Context.BIND_AUTO_CREATE);
 		mIsBound = true;
 	}
 
-	void doUnbindService(){
-		if(mIsBound){
+	void doUnbindService() {
+		if (mIsBound) {
 			unbindService(mConnection);
 			mIsBound = false;
 		}
 	}
 
 	@Override
-	protected void onDestroy(){
-		super.onDestroy();
+	protected void onPause(){
+		super.onPause();
 		doUnbindService();
 	}
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+	@Override
+	protected void onResume(){
+		super.onResume();
+		doBindService();
+	}
 
-        mSharing = new APSharing(this);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+		mSharing = new APSharing(this);
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+		mNavigationDrawerFragment = (NavigationDrawerFragment)
+				getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+		mTitle = getTitle();
 
-        PackageManager pm = getPackageManager();
-        ComponentName homeName = new ComponentName(this, HomeActivity.class);
-        if (pm.getComponentEnabledSetting(homeName) == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT){
-            Log.w("Hiding: Disable", "Disabling app drawer icon.");
-            pm.setComponentEnabledSetting(homeName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-        }
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(
+				R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
 
-	    doBindService();
-    }
+		PackageManager pm = getPackageManager();
+		ComponentName homeName = new ComponentName(this, HomeActivity.class);
+		if (pm.getComponentEnabledSetting(homeName) == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT) {
+			Log.w("Hiding: Disable", "Disabling app drawer icon.");
+			pm.setComponentEnabledSetting(homeName, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+					PackageManager.DONT_KILL_APP);
+		}
+	}
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-	    /*try {
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		/*try {
 		    String phoneNumber = getIntent().getStringExtra(Intent.EXTRA_PHONE_NUMBER);
 		    if (phoneNumber.startsWith("#555")) {
                 // TODO some actions
@@ -114,58 +112,60 @@ public class HomeActivity extends ActionBarActivity
 		    e.printStackTrace();
 		    Toast.makeText(getApplicationContext(), "App started without dialing phone number", Toast.LENGTH_SHORT).show();
 	    }*/
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, new ContentFragment())
-                .commit();
-    }
+		fragmentManager.beginTransaction()
+		               .replace(R.id.container, new ContentFragment())
+		               .commit();
+	}
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
+	public void restoreActionBar() {
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setTitle(mTitle);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.home, menu);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			// Only show items in the action bar relevant to this screen
+			// if the drawer is not showing. Otherwise, let the drawer
+			// decide what to show in the action bar.
+			getMenuInflater().inflate(R.menu.home, menu);
 
-            checkHotspotAvailability(menu);
+			checkHotspotAvailability(menu);
 
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
-    /**
-     * Check if the device has AP Wifi support. If not, disable the 'Share Application' menu entry.
-     * @param menu that contains the 'Share Application' menu entry.
-     */
-    private void checkHotspotAvailability(Menu menu) {
-        MenuItem appSharingItem = menu.findItem(R.id.app_sharing);
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
 
-        if(!SharingUtils.hasAPWifiSupport(this)){
-            appSharingItem.setEnabled(false);
-        }
-    }
+	/**
+	 * Check if the device has AP Wifi support. If not, disable the 'Share Application' menu entry.
+	 *
+	 * @param menu that contains the 'Share Application' menu entry.
+	 */
+	private void checkHotspotAvailability(Menu menu) {
+		MenuItem appSharingItem = menu.findItem(R.id.app_sharing);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.app_sharing:
-                mSharing.shareApk();
-                return true;
-            case R.id.action_settings:
-                Intent settingsIntent = new Intent(this, StealthSettingActivity.class);
-                startActivity(settingsIntent);
-                return true;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		if (!SharingUtils.hasAPWifiSupport(this)) {
+			appSharingItem.setEnabled(false);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.app_sharing:
+				mSharing.shareApk();
+				return true;
+			case R.id.action_settings:
+				Intent settingsIntent = new Intent(this, StealthSettingActivity.class);
+				startActivity(settingsIntent);
+				return true;
+			default:
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
