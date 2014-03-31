6fce6c1078d485bfdeff99abe6d7553a34c3817c
Olivier Hokke <github@wolfox.nl>
2014-03-31 18:57:05 +0200

    Now the widget only opens the pin dialog when the user presses 5 times on the widget. Idea: dont just use an amount of times pressed, but use a morse/beat/musical sequence in order to open the pin dialog. Should be unrelated to speed, so one should use percentages of clicks intervals based on the previous.

--- AndroidStealth/src/main/java/com/stealth/android/StealthButton.java
+++ AndroidStealth/src/main/java/com/stealth/android/StealthButton.java
@@ -7,12 +7,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.stealth.utils.Utils;
+
 import pin.PinActivity;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class StealthButton extends AppWidgetProvider {
+
+    public static final String ACTION_BUTTON_PRESS = "buttonPress";
+    private static final long TOUCH_INTERVAL = 1500;
+    private static final int CLICKS_TO_LAUNCH = 4;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -36,14 +42,43 @@ public class StealthButton extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
             int appWidgetId) {
+        Utils.setContext(context);
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stealth_button);
-        Intent intent = new Intent(context, PinActivity.class);
-        views.setOnClickPendingIntent(R.id.toggleButton, PendingIntent.getActivity(context, 0, intent, 0));
+        Intent intent = new Intent(context, StealthButton.class);
+        intent.setAction(ACTION_BUTTON_PRESS);
+        views.setOnClickPendingIntent(R.id.toggleButton, PendingIntent.getBroadcast(context, 0, intent, 0));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private static long mLastTime = 0;
+    private static int mClicks = 0;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction() != null && intent.getAction().equals(ACTION_BUTTON_PRESS)) {
+            long now = System.currentTimeMillis();
+            long diff = now - mLastTime;
+
+            if (diff < TOUCH_INTERVAL) {
+                mClicks++;
+            } else {
+                mClicks = 0;
+            }
+
+            mLastTime = now;
+
+            if (CLICKS_TO_LAUNCH == mClicks) {
+                Intent pinIntent = new Intent(context, PinActivity.class);
+                pinIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(pinIntent);
+                mClicks = 0;
+            }
+        }
     }
 }
 
