6586bb518a1289f298daff5787dba6739cf06ce4
Alex <a_kolpa@live.nl>
2014-04-24 20:06:00 +0200

    Added workaround for typeface loading crash

--- AndroidStealth/src/main/java/com/stealth/font/FontManager.java
+++ AndroidStealth/src/main/java/com/stealth/font/FontManager.java
@@ -1,5 +1,8 @@
 package com.stealth.font;
 
+import java.util.Hashtable;
+
+import android.content.Context;
 import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,8 +10,7 @@ import android.widget.TextView;
 import com.stealth.utils.Utils;
 
 /**
- * Manages and sets the fonts
- * Created by OlivierHokke on 23-Apr-14.
+ * Manages and sets the fonts Created by OlivierHokke on 23-Apr-14.
  */
 public class FontManager {
 
@@ -16,53 +18,44 @@ public class FontManager {
 	public static final String TAG_LIGHT_CONDENSED = "LightCondensed";
 	public static final String TAG_REGULAR = "Regular";
 
-	private static Typeface sBoldCondensed;
-	private static Typeface sLightCondensed;
-	private static Typeface sRegular;
-	private static boolean sInitialized = false;
-	private static void initialize() {
-		if (!sInitialized){
-			sBoldCondensed = Typeface.createFromAsset(Utils.getContext().getAssets(), "fonts/YanoneKaffeesatz-Bold.ttf");
-			sLightCondensed = Typeface.createFromAsset(Utils.getContext().getAssets(), "fonts/YanoneKaffeesatz-Light.ttf");
-			sRegular = Typeface.createFromAsset(Utils.getContext().getAssets(), "fonts/RobotoSlab-Light.ttf");
-			sInitialized = true;
-		}
-	}
+	public static final String BOLD_CONDENSED_PATH = "fonts/YanoneKaffeesatz-Bold.ttf";
+	public static final String LIGHT_CONDENSED_PATH = "fonts/YanoneKaffeesatz-Light.ttf";
+	public static final String REGULAR_PATH = "fonts/RobotoSlab-Light.ttf";
 
 	/**
 	 * Sets the typeface for the given textview to the bold condensed one
+	 *
 	 * @param v the textview to set
 	 */
 	public static void setBoldCondensed(TextView v) {
-		initialize();
-		v.setTypeface(sBoldCondensed);
+		v.setTypeface(Typefaces.get(Utils.getContext(), BOLD_CONDENSED_PATH));
 	}
 
 	/**
 	 * Sets the typeface for the given textview to the light condensed one
+	 *
 	 * @param v the textview to set
 	 */
 	public static void setLightCondensed(TextView v) {
-		initialize();
-		v.setTypeface(sLightCondensed);
+		v.setTypeface(Typefaces.get(Utils.getContext(), LIGHT_CONDENSED_PATH));
 	}
 
 	/**
 	 * Sets the typeface for the given textview to the regular one
+	 *
 	 * @param v the textview to set
 	 */
 	public static void setRegular(TextView v) {
-		initialize();
-		v.setTypeface(sRegular);
+		v.setTypeface(Typefaces.get(Utils.getContext(), REGULAR_PATH));
 	}
 
 	/**
-	 * Go through entire hierarchy and finds textviews to set the font of, but will
-	 * only do so with the views that have special font tags
+	 * Go through entire hierarchy and finds textviews to set the font of, but will only do so with the views that have
+	 * special font tags
+	 *
 	 * @param v the view group to traverse
 	 */
 	public static void handleFontTags(View v) {
-		initialize();
 		if (v instanceof TextView) {
 			Object tag = v.getTag();
 			if (tag == null) {
@@ -79,9 +72,33 @@ public class FontManager {
 			}
 		}
 		if (v instanceof ViewGroup) {
-			ViewGroup vg = (ViewGroup)v;
+			ViewGroup vg = (ViewGroup) v;
 			for (int i = 0; i < vg.getChildCount(); i++) {
 				handleFontTags(vg.getChildAt(i));
+			}
+		}
+	}
+
+	/**
+	 * Helper class to retrieve fonts from
+	 */
+	private static class Typefaces {
+		private static final Hashtable<String, Typeface> cache = new Hashtable<String, Typeface>();
+
+		public static Typeface get(Context c, String assetPath) {
+			synchronized (cache) {
+				if (!cache.containsKey(assetPath)) {
+					try {
+						Typeface t = Typeface.createFromAsset(c.getAssets(),
+								assetPath);
+						cache.put(assetPath, t);
+					} catch (Exception e) {
+						Utils.d("Could not get typeface '" + assetPath
+								+ "' because " + e.getMessage());
+						return null;
+					}
+				}
+				return cache.get(assetPath);
 			}
 		}
 	}
