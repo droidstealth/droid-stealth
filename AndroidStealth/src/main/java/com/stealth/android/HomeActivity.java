02aa15264ea84a11f3ecff39eaf32f6d2a6a93f2
Alex Walterbos <atw.231@gmail.com>
2015-02-12 10:14:41 +0100

    Fixed dependency on droidmorphing-lib git submodule

--- AndroidStealth/src/main/java/com/stealth/android/HomeActivity.java
+++ AndroidStealth/src/main/java/com/stealth/android/HomeActivity.java
@@ -4,11 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
